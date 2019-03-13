712d9456844b0b59d2638ef76fdaf6c18f56af1e
Etienne <etienne.arlaud@free.fr>
2019-03-13 09:20:32 +0100

    Reduction of main

--- wifiRawEcho/src/main.c
+++ wifiRawEcho/src/main.c
@@ -20,24 +20,29 @@ Etienne Arlaud
 static uint8_t src_mac[6] = {0xF8, 0x1A, 0x67, 0xb7, 0xEB, 0x0B};
 static uint8_t dest_mac[6] = {0x84, 0xf3, 0xeb, 0x73, 0x55, 0x0d};
 
+ESPNOW_packet my_packet;
+uint8_t raw_bytes[400];
 
 int create_raw_socket(char *dev)
 {
-    struct sockaddr_ll s_dest_addr; //code from sender
+    struct sockaddr_ll s_dest_addr;
     struct ifreq ifr;
-    int fd, ifi, rb;
-
-    bzero(&s_dest_addr, sizeof(s_dest_addr));
+	
+    int fd, 	//file descriptor
+		ioctl_errno,	//ioctl errno
+		bind_errno;		//bind errno
+	
+	bzero(&s_dest_addr, sizeof(s_dest_addr));
     bzero(&ifr, sizeof(ifr));
-
-    (void)memset(&s_dest_addr, 0, sizeof(s_dest_addr));
-
+	
+	
     fd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-    assert(fd != -1); //abort if error
+    assert(fd != -1); 			//abort if error
 
-    strncpy((char *)ifr.ifr_name, dev, IFNAMSIZ);
-    ifi = ioctl(fd, SIOCGIFINDEX, &ifr);
-    assert(ifi != -1); //abort if error
+    strncpy((char *)ifr.ifr_name, dev, IFNAMSIZ); //interface
+
+    ioctl_errno = ioctl(fd, SIOCGIFINDEX, &ifr);
+    assert(ioctl_errno >= 0);	//abort if error
 
     s_dest_addr.sll_family = PF_PACKET;
     //we don't use a protocol above ethernet layer, just use anything here
@@ -46,19 +51,10 @@ int create_raw_socket(char *dev)
     s_dest_addr.sll_hatype = ARPHRD_ETHER;
     s_dest_addr.sll_pkttype = PACKET_OTHERHOST; //PACKET_OUTGOING
     s_dest_addr.sll_halen = ETH_ALEN;
-    //MAC - begin
-    s_dest_addr.sll_addr[0] = dest_mac[0];
-    s_dest_addr.sll_addr[1] = dest_mac[1];
-    s_dest_addr.sll_addr[2] = dest_mac[2];
-    s_dest_addr.sll_addr[3] = dest_mac[3];
-    s_dest_addr.sll_addr[4] = dest_mac[4];
-    s_dest_addr.sll_addr[5] = dest_mac[5];
-    //MAC - end
-    s_dest_addr.sll_addr[6] = 0x00; //not used
-    s_dest_addr.sll_addr[7] = 0x00; //not used
+	memcpy(&(s_dest_addr.sll_addr),dest_mac, sizeof(*src_mac)*6); //MAC
 
-    rb = bind(fd, (struct sockaddr *)&s_dest_addr, sizeof(s_dest_addr));
-    assert(rb != -1); //abort if error
+    bind_errno = bind(fd, (struct sockaddr *)&s_dest_addr, sizeof(s_dest_addr));
+    assert(bind_errno >= 0);	//abort if error
 
     return fd;
 }
@@ -78,36 +74,36 @@ int main(int argc, char **argv)
 
     char *dev = argv[1];
 
+	int packets_sent = 0;	
+	
     int sock_fd = -1;
     int s32_res = -1;
 
     sock_fd = create_raw_socket(dev); /* Creating the raw socket */
 
-    if (-1 == sock_fd)
+    if (sock_fd < 0)
     {
         perror("Could not create the socket");
         goto LABEL_CLEAN_EXIT;
-    }
-
-    printf("Socket created\n");
+    } else {
+		printf("Socket created\n");
+	}
 
     fflush(stdout);
 
     sleep(1);
+	
 
-    int Counter = 0;
-	ESPNOW_packet my_packet;
 	init_ESPNOW_packet(&my_packet);
 	memcpy(my_packet.wlan.da, dest_mac, sizeof(uint8_t)*6);
 	memcpy(my_packet.wlan.sa, src_mac, sizeof(uint8_t)*6);
 	memcpy(my_packet.wlan.bssid, dest_mac, sizeof(uint8_t)*6);
-	uint8_t raw_bytes[400];
 	int len = packet_to_bytes(raw_bytes, 400, my_packet);
 
     while (1)
     {
 
-            printf("Send : %d\n", Counter++);
+            printf("Send : %d\n", packets_sent++);
             s32_res = send_echo(sock_fd, raw_bytes, len);
 			print_raw_packet(raw_bytes, len);
 
