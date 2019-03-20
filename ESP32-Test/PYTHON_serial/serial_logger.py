24212ae8c660f681aaa9cfaf30a4395d8db874df
Etienne <etienne.arlaud@free.fr>
2019-03-20 16:31:29 +0100

    Pyton log with plots

--- ESP32-Test/PYTHON_serial/serial_logger.py
+++ ESP32-Test/PYTHON_serial/serial_logger.py
@@ -1,6 +1,7 @@
 import serial
 import sys
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 assert len(sys.argv)==3
 
@@ -9,21 +10,25 @@ log_file = open(sys.argv[2],"a+")
 ser = serial.Serial(sys.argv[1], 115200)
 ser.flushInput()
 
+n_plot = 0
+
 print "Serial opened : " + str(sys.argv[1])
 
-def read():
-	line = ser.readline()
+def read(display=True):
+	line = ser.readline().rstrip()
 	log_file.write(line)
+	if(display):
+		print('\t' + line)
 	return line
 
 def read_int(i):
-	return int(read().rstrip().split('\t')[i])
+	return int(read().split('\t')[i])
 
 def decode_histo():
-	units = read().rstrip().split('\t')[1]
-	bounds = list(map(int, read().rstrip().split('\t')[1:]))
+	units = read().split('\t')[1]
+	bounds = list(map(int, read().split('\t')[1:]))
 	nb_values = read_int(1)
-	histo = list(map(int, read().rstrip().split('\t')[1:]))
+	histo = list(map(int, read(False).split('\t')[1:]))
 	avg = read_int(1)
 	recv = read_int(1)
 	sent = read_int(1)
@@ -32,8 +37,9 @@ def decode_histo():
 	histo = histo[:-1]
 	nb_values = nb_values-1
 
+	bounds = list(map(lambda x : x/1000., bounds))
 	x_axis = [(x+0.5)*(bounds[1]-bounds[0])/nb_values+bounds[0] for x in range(nb_values)]
-	x_axis = list(map(lambda x : x/1000., x_axis))
+	
 
 	histo_cumulate = [0 for i in range(len(histo))]
 	histo_cumulate[0] = histo[0]
@@ -43,38 +49,45 @@ def decode_histo():
 	histo = list(map(lambda x:x*100./sent, histo))
 	histo_cumulate = list(map(lambda x: x*100./sent, histo_cumulate))
 
-	fig, ax1 = plt.subplots()
+	plt.ion()
+
+	fig = plt.figure(n_plot)
+	ax1 = fig.add_subplot(111)
 	ax2 = ax1.twinx()
 
-	plt.title('Histogram')
+	plt.title('(' + str(n_plot) + ')' + 'Overall receive ' + str(recv*100./sent) + '% ; Sent : ' + str(sent) + ' ; Longer than ' + str(bounds[1]) + units + ' : ' + str(overtimed))
 
 	ax1.plot(x_axis, histo, color='tab:blue')
 	ax1.set_ylabel('% packets', color='tab:blue')
 	ax1.tick_params(axis='y', labelcolor='tab:blue')
+	ax1.set_yticks([i*0.5  for i in range(int(max(histo)+1)*2)])
+
+	ax1.set_xticks([i*(bounds[1]-bounds[0])/10.+bounds[0]  for i in range(11)])
+
 
 	ax2.plot(x_axis, histo_cumulate, color='tab:red')
 	ax2.set_ylabel('% packets', color='tab:red')
 	ax2.tick_params(axis='y', labelcolor='tab:red')
+	ax2.set_yticks([i*10  for i in range(11)])
 
 	ax2.plot([x_axis[0], x_axis[-1]], [95, 95], color='tab:orange')
 
-	ax1.set_xlabel('Delay (' + units + ')')
+	ax1.set_xlabel('Delay ( 1000' + units + ') ; Avg = ' + str(avg) + units)
 
 	fig.tight_layout()
-	fig.legend()
+	fig.legend(["histogram", "cumulated", "95%", "hello"])
+	plt.grid()
 
-	plt.show()
+	plt.plot()
 
 
-while True:
-		#try:
-		line = read()
-		if("----------" in line):
-			decode_histo()
-			break
-		else:
-			print("pas hello")
-		#except:
-		print("Keyboard Interrupt")
-		#break
+while n_plot < 3:
+	line = read(False)
+	if("----------" in line):
+		n_plot+=1
+		print("\033[31mHisto " + str(n_plot) + " \033[31mreceived\033[0m")
+		decode_histo()
+	else:
+		print("\033[34mLine not recognized \033[0m >> \033[33m" + line + "\033[0m")
 	
+raw_input("Press [enter] to continue.")
